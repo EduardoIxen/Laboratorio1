2b5f7d5ffb70d01b93b20facb709556cbea0a615
EduardoIxen <tomas-1539@hotmail.com>
2016-05-13 21:15:36 -0600

    Laboratorio1

---
+++ app/src/main/java/com/example/dell/laboratorio1/Principal.java
@@ -0,0 +1,30 @@
+package com.example.dell.laboratorio1;
+
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Principal extends AppCompatActivity {
+    private EditText txtPedir;
+    private Button btnPedir;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+        txtPedir=(EditText) findViewById(R.id.txtPedir);
+        btnPedir=(Button) findViewById(R.id.btnPedir);
+        btnPedir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Snackbar.make(v, "Click en boton pedido", Snackbar.LENGTH_SHORT).show();
+                Intent intent = new Intent(Principal.this, Factura.class);
+                intent.putExtra("pedido",txtPedir.getText().toString());
+                startActivity(intent);
+            }
+        });
+    }
+}
